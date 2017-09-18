5be46a20fe09200c2ff7b9248d277cb9fdf8d5a6
ignacio <Ignaciovl.j@gmail.com>
2017-09-18 10:58:53 -0700

    añadiendo fisicas y jugabilidad

--- src/app/simplegame/simplegame.component.ts
+++ src/app/simplegame/simplegame.component.ts
@@ -13,6 +13,9 @@ export class SimplegameComponent{
   private ground:Phaser.Sprite;
   private player:Phaser.Sprite;
 
+  //cursor
+  private cursors:Phaser.CursorKeys;
+
   //platform group
   private platforms:Phaser.Group;
    //Phaser Game and World
@@ -25,7 +28,9 @@ export class SimplegameComponent{
 
   constructor() {
     this.game = new Phaser.Game(this.ancho,this.alto, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, update:this.update });
-    this.world=this.game.world;}
+    this.world=this.game.world;
+      
+  }
 
 
 
@@ -79,10 +84,44 @@ private create() {
     //  Our two animations, walking left and right.
     this.player.animations.add('left', [0, 1, 2, 3], 10, true);
     this.player.animations.add('right', [5, 6, 7, 8], 10, true);
-    this.player.body.gravity.y=300;   
+    this.player.body.gravity.y=400;
+
+    //inicializando el teclado xD
+    this.cursors=this.game.input.keyboard.createCursorKeys();
 }
-  update(){
-    this.game.physics.arcade.collide(this.player, this.platforms);
+  private update(){
+    var hitPlatform= this.game.physics.arcade.collide(this.player, this.platforms);
+
+    //  Reset the players velocity (movement)
+    this.player.body.velocity.x = 0;
+    
+        if (this.cursors.left.isDown)
+        {
+            //  Move to the left
+            this.player.body.velocity.x = -150;
+    
+            this.player.animations.play('left');
+        }
+        else if (this.cursors.right.isDown)
+        {
+            //  Move to the right
+            this.player.body.velocity.x = 150;
+    
+            this.player.animations.play('right');
+        }
+        else
+        {
+            //  Stand still
+            this.player.animations.stop();
+    
+            this.player.frame = 4;
+        }
+    
+        //  Allow the player to jump if they are touching the ground.
+        if (this.cursors.up.isDown && this.player.body.touching.down && hitPlatform)
+        {
+          this.player.body.velocity.y = -350;
+        }
   }
 
 }
