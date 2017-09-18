c78f1a6c5ef2cfbdeaf14fd6cdf89211c5b82dcf
ignacio <Ignaciovl.j@gmail.com>
2017-09-18 09:16:53 -0700

    Part 4 - Groups

--- src/app/simplegame/simplegame.component.ts
+++ src/app/simplegame/simplegame.component.ts
@@ -7,21 +7,62 @@ import * as Phaser from "phaser-ce";
 })
 export class SimplegameComponent{
 
+  //Sprites
+  private star:Phaser.Sprite;
+  private sky:Phaser.Sprite;
+  private ground:Phaser.Sprite;
+
+  //platform group
+  private platforms:Phaser.Group;
+   //Phaser Game and World
+   private game: Phaser.Game;
+   private world:Phaser.World;
+   private ancho:number=800;
+   private alto:number=600;
+   
+
+
   constructor() {
-    this.game = new Phaser.Game(900, 700, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+    this.game = new Phaser.Game(this.ancho,this.alto, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+    this.world=this.game.world;}
+
+
+
+
+private preload() {
+  this.game.load.image('sky', 'assets/sky.png');
+  this.game.load.image('ground', 'assets/platform.png');
+  this.game.load.image('star', 'assets/star.png');
+  this.game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 }
 
-game: Phaser.Game;
 
-preload() {
-    this.game.load.image('logo', 'assets/phaser2.png');
-}
+private create() {
+  //  We're going to be using physics, so enable the Arcade Physics system
+   this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-create() {
-    var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-    logo.anchor.setTo(0.5, 0.5);
-    logo.scale.setTo(0.2, 0.2);
-    this.game.add.tween(logo.scale).to({ x: 1, y: 1 }, 2000, Phaser.Easing.Bounce.Out, true);
+   this.sky=this.game.add.sprite(0, 0, 'sky');
+   this.star=this.game.add.sprite(0, 0, 'star');
+    //  The platforms group contains the ground and the 2 ledges we can jump on
+   this.platforms=this.game.add.group();
+      //  We will enable physics for any object that is created in this group
+   this.platforms.enableBody=true;
+   this.ground=this.platforms.create(0, this.world.height - 64, 'ground');
+    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+    this.ground.scale.setTo(2, 2);
+    this.ground.body.immovable = true;
+
+    /*  Now let's create two ledges.
+     Nota: Crea un sprite local que se asigna a un grupo.A ese Sprite local
+    se le setea la propiedad, luego se sobre-escribe, y se vuelve a setear una propiedad pero esta
+    vez al sprite sobre escrito por eso si quisieramos un mayor manejo de los sprites que asignamos a
+    un grupo lo que podríamos hacer sería crear un arreglo de sprites, agregarlos a un grupo y así mantener
+    la referencia de los sprites asignados a ese grupo en ese arreglo y poder manipularlos con mayor libertad.*/
+    let ledge:Phaser.Sprite;
+    ledge = this.platforms.create(400, 400, 'ground');   
+    ledge.body.immovable = true;
+    ledge = this.platforms.create(-140, 240, 'ground');
+    ledge.body.immovable = true;
 }
 
 
